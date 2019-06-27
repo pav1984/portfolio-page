88260022076b48ecc5fa92e85dd28973106483d4
Pawel Oczkowski <pawel.oczko84@gmail.com>
2019-06-27 21:32:40 +0200

    Add Landspace orientation

--- js/script.js
+++ js/script.js
@@ -110,16 +110,3 @@ sr.reveal('#contact', {
 	origin: 'top',
 	distance: '50px'
 });
-
-(function(d) {
-	var w = d.documentElement.offsetWidth,
-		t = d.createTreeWalker(d.body, NodeFilter.SHOW_ELEMENT),
-		b;
-	while (t.nextNode()) {
-		b = t.currentNode.getBoundingClientRect();
-		if (b.right > w || b.left < 0) {
-			t.currentNode.style.setProperty('outline', '1px dotted red', 'important');
-			console.log(t.currentNode);
-		}
-	}
-})(document);

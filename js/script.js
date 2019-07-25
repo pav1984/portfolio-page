532da7d0f4308a2da543e313df026a9120cdcca9
Pawel Oczkowski <pawel.oczko84@gmail.com>
2019-07-25 10:37:11 +0200

    Fixed some bugs in html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// GLOBAL VARIABLES
 const btnMenu = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const leftSideMenu = document.querySelector('.menu-left-side');
